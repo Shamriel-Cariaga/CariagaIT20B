d59a29f850ae4a543e0c59ef4b8a64bca558384d
Shamriel <shamriel614@gmail.com>
2026-09-09 19:02:52 +0800

    Add files via upload

---
+++ CARIAGA_DSA_IT20B/src/singledimensionalarray/singleDimension.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package singledimensionalarray;
+
+/**
+ *
+ * @author User
+ */
+public class singleDimension {
+   public static void main(String[] args){
+       
+       int [] values = {10, 20, 30, 40, 50};
+       
+        for (int i = 0; i < values.length; i++) {
+        System.out.print(values[i] + " ");
+        
+        }
+   }
+}
